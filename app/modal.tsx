65617f4e65ff990fb0983bb5d9a38957665d6f85
loveyounie <loveyounie@naver.com>
2025-12-30 20:53:45 +0900

    initialize

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,17 +1,18 @@
 import { Link } from 'expo-router';
 import { StyleSheet } from 'react-native';
+import { Text, View } from 'react-native-reanimated/lib/typescript/Animated';
 
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
 
 export default function ModalScreen() {
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
-    </ThemedView>
+    <>
+      <View style={styles.container}>
+        <Text >This is a modal</Text>
+        <Link href="/" dismissTo style={styles.link}>
+          <Text>Go to home screen</Text>
+        </Link>
+      </View>
+    </>
   );
 }
 
